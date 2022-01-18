0668ca3a83acf8dcee9dd54c058acdfaa983fbea
junuMoon <francomoon7@gmail.com>
2022-01-18 22:34:42 +0900

    Update Model: calculate preferences

--- food_recommendation/src/model.py
+++ food_recommendation/src/model.py
@@ -4,8 +4,23 @@ class Model:
         """
         Initialize the model
         """
-        self.menu = ['pizza', 'pasta', 'ramen', 'fried_rice', 'ttoeokbokki']
+        self.menu = ['pizza', 'pasta', 'ramen', 'fried_rice', 'ttoeokbokki', 'jjiigae']
         self.data = data
+
+    @property
+    def flat_data(self):
+        return [item for sublist in self.data.values() for item in sublist]
+
+    def count_menu(self, data):
+        return {i: data.count(i) for i in range(len(self.menu))}
+
+    def preferences(self, name):
+        """
+        Get the preferences of the people
+        """
+        all_menu_count = self.count_menu(self.flat_data)
+        person_menu_count = self.count_menu(self.data[name])
+        return {i: person_menu_count[i] / all_menu_count[i] for i in range(len(self.menu))}
 
     def recommend(self, name):
         """
@@ -13,5 +28,5 @@ class Model:
         """
         assert name in self.data.keys(), \
             'The name is not in the data'
-        menu_idx = max({i: self.data[name].count(i) for i in range(1, 6)}.items(), key=lambda x: x[1])[0] - 1
-        return self.menu[menu_idx]
+        preferences = self.preferences(name)
+        return self.menu[max(preferences.items(), key=lambda x: x[1])[0]]

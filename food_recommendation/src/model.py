235e23181f0edd34e5239ba44f055735190d68a4
junuMoon <francomoon7@gmail.com>
2022-01-18 21:37:47 +0900

    Fix: Model should return menu name

--- food_recommendation/src/model.py
+++ food_recommendation/src/model.py
@@ -1,25 +1,15 @@
-from enum import Enum
-
-class Menu(Enum):
-    """
-    Enum for the different menus
-    """
-    pizza = 1
-    pasta = 2
-    ramen = 3
-    fried_rice = 4
-    ttoeokbokki = 5
-
 class Model:
 
     def __init__(self, data):
         """
         Initialize the model
         """
+        self.menu = ['pizza', 'pasta', 'ramen', 'fried_rice', 'ttoeokbokki']
         self.data = data
 
     def recommend(self):
         """
         Recommend a food
         """
-        return max({i: self.data.count(i) for i in range(1, 6)}.items(), key=lambda x: x[1])[0]
+        menu_idx = max({i: self.data.count(i) for i in range(1, 6)}.items(), key=lambda x: x[1])[0] - 1
+        return self.menu[menu_idx]
--- food_recommendation/tests/test_model.py
+++ food_recommendation/tests/test_model.py
@@ -9,12 +9,12 @@ def one_person_data():
     """
     return [random.randint(1, 5) for _ in range(10)]
 
-def test_model_random_selection(one_person_data):
+def test_model_recommend_food(one_person_data):
     """
     Test the random selection of the food
     """
     model = Model(one_person_data)
-    assert model.recommend() is not None
+    assert type(model.recommend()) == str
 
 def test_model_recommend_most_common(one_person_data):
     """
@@ -22,5 +22,14 @@ def test_model_recommend_most_common(one_person_data):
     """
     model = Model(one_person_data)
     print(one_person_data)
-    most_common = max({i: one_person_data.count(i) for i in range(1, 6)}.items(), key=lambda x: x[1])[0]
+    most_common = model.menu[max({i: one_person_data.count(i) for i in range(1, 6)}.items(), key=lambda x: x[1])[0] - 1]
     assert model.recommend() == most_common
+
+@pytest.fixture
+def two_person_data():
+    """
+    Fixture for a two person data
+    """
+    return {'Jason': [random.randint(1, 5) for _ in range(10)],
+            'Kim': [random.randint(1, 5) for _ in range(10)]}
+

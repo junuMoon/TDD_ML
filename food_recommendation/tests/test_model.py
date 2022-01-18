e0ed68ecfe1c24f83221cc804d0e1633cf6b5b19
junuMoon <francomoon7@gmail.com>
2022-01-18 22:16:03 +0900

    Issue: zerodivisionError

--- food_recommendation/tests/test_model.py
+++ food_recommendation/tests/test_model.py
@@ -7,7 +7,7 @@ def one_person_data():
     """
     Fixture for a one person data
     """
-    return [random.randint(0, 5) for _ in range(10)]
+    return [random.randint(0, 5) for _ in range(5)]
 
 def test_model_recommend_food(one_person_data):
     """
@@ -49,6 +49,8 @@ def test_model_recommend_most_common(two_person_data):
     Test the recommendation of the most common food
     """
     model = Model(two_person_data)
+    all_menu_count = model.count_menu(model.flat_data)
+    print(all_menu_count) # FIXME: ZeroDivisonError when any menu count is 0
     assert model.recommend('Jason') == 'pasta'
 
 @pytest.fixture
@@ -56,16 +58,30 @@ def ten_people_data():
     """
     Fixture for a ten person data
     """
-    return {'Jason': [random.randint(0, 5) for _ in range(10)],
-            'Kim': [random.randint(0, 5) for _ in range(10)],
-            'Linda': [random.randint(0, 5) for _ in range(10)],
-            'Mason': [random.randint(0, 5) for _ in range(10)],
-            'Nathan': [random.randint(0, 5) for _ in range(10)],
-            'Olivia': [random.randint(0, 5) for _ in range(10)],
-            'Pam': [random.randint(0, 5) for _ in range(10)],
-            'Quinn': [random.randint(0, 5) for _ in range(10)],
-            'Riley': [random.randint(0, 5) for _ in range(10)],
-            'Sam': [random.randint(0, 5) for _ in range(10)]}
+    data = {'Jason': [random.randint(0, 5) for _ in range(10)],
+            'Kim': [random.randint(0, 5) for _ in range(5)],
+            'Linda': [random.randint(0, 5) for _ in range(5)],
+            'Mason': [random.randint(0, 5) for _ in range(5)],
+            'Nathan': [random.randint(0, 5) for _ in range(5)],
+            'Olivia': [random.randint(0, 5) for _ in range(5)],
+            'Pam': [random.randint(0, 5) for _ in range(5)],
+            'Quinn': [random.randint(0, 5) for _ in range(5)],
+            'Riley': [random.randint(0, 5) for _ in range(5)],
+            'Sam': [random.randint(0, 5) for _ in range(5)]}
+
+    for _ in range(5):
+        data['Jason'].append(1)
+        data['Kim'].append(0)
+        data['Linda'].append(2)
+        data['Mason'].append(3)
+        data['Nathan'].append(4)
+        data['Olivia'].append(5)
+        data['Pam'].append(4)
+        data['Quinn'].append(3)
+        data['Riley'].append(2)
+        data['Sam'].append(0)
+
+    return data
 
 
 def test_model_recommend_his_favorite_menu(ten_people_data):
@@ -74,9 +90,5 @@ def test_model_recommend_his_favorite_menu(ten_people_data):
     favorite menu is calculated by the most common food / the number all people choice that menu
     """
     model = Model(ten_people_data)
-    menu_count_1dim = {i: ten_people_data['Jason'].count(i) for i in range(0, 5)}
-    flat_data = []
-    [flat_data.extend(data) for data in ten_people_data.values()]
-    menu_count_2dim = {i: flat_data.count(i) for i in range(0, 5)}
-    favorite_menu_idx = max({i: menu_count_1dim[i] / menu_count_2dim[i] for i in range(0, 5)}.items(), key=lambda x: x[1])[0] - 1
-    assert model.recommend('Jason') == model.menu[favorite_menu_idx]
+    print(model.preferences('Jason'))
+    assert model.recommend('Jason') == 'pasta'

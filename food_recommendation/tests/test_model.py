75bbcad709b756f8674d2abbcf48decdfaaadb09
junuMoon <francomoon7@gmail.com>
2022-01-18 21:27:36 +0900

    Test Passed Model recommend some food anyway

--- food_recommendation/tests/test_model.py
+++ food_recommendation/tests/test_model.py
@@ -1,9 +1,17 @@
 from src.model import Model
 import pytest
+import random
 
-def test_model_random_selection():
+@pytest.fixture
+def one_person_data():
+    """
+    Fixture for a one person data
+    """
+    return [random.randint(1, 5) for _ in range(10)]
+
+def test_model_random_selection(one_person_data):
     """
     Test the random selection of the food
     """
-    model = Model()
+    model = Model(one_person_data)
     assert model.recommend() is not None

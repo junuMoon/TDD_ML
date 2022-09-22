3843510feaba1d3e512b45dbec7856b991bd5b7c
Fran Moon <francomoon7@gmail.com>
2022-09-22 10:32:06 +0900

    Build decoder

--- rnn-t/src/model.py
+++ rnn-t/src/model.py
@@ -1,13 +1,40 @@
 import torch.nn as nn
 import torch
+
 class Encoder(nn.Module):
 
     def __init__(self, num_inputs, output_dim, hidden_state_dim=64) -> None:
         super().__init__()
-        self.rnn = nn.GRU(num_inputs, hidden_state_dim, batch_first=True)
+        self.rnn = nn.GRU(num_inputs, hidden_state_dim)
         self.fc = nn.Linear(hidden_state_dim, output_dim)
 
     def forward(self, x):
         output, _ = self.rnn(x)
         return self.fc(output)
+
+class Decoder(nn.Module):
+    """The decoder is any causal network (= can't look at the future)"""
+
+    def __init__(self, num_classes, output_dim, hidden_state_dim=64) -> None:
+        super().__init__()
+        self.emb = nn.Embedding(num_classes, hidden_state_dim)
+        self.rnn = nn.RNN(hidden_state_dim, hidden_state_dim)
+        self.fc = nn.Linear(hidden_state_dim, output_dim)
+        
+        self.initial_state = nn.Parameter(torch.randn(1, hidden_state_dim))
+
+    def step(self, yi, prev_state):
+        embedded = self.emb(yi)
+        output, state = self.rnn.forward(embedded, prev_state)
+        return output, state
+
+    def forward(self, y):
+        U = y.shape[1]
+        g = []
+        prev_state = self.initial_state
+        for i in range(U):
+            yi = y[:, i]
+            g_u, prev_state = self.step(yi, prev_state)
+            g.append(g_u)
+        return torch.cat(g, dim=0)
 
--- rnn-t/tests/test_model.py
+++ rnn-t/tests/test_model.py
@@ -1,6 +1,6 @@
 import pytest
 import torch
-
+import torch.nn.functional as  F
 from src.model import Encoder, Decoder, Joiner
 
 
@@ -16,3 +16,24 @@ def test_encoder():
     f = encoder(x)
     assert f.shape == encoder_output_shape
 
+class TestDecoder:
+    
+    def setup(self):
+        seq_len = 5  # U
+        num_vocab = 4  # V: {v1, v2, v3, v4}
+        decoder_dim = 64  # D
+
+        self.decoder_input_shape = (seq_len+1, num_vocab+1)  # (U+1, V+1), V+1 because of the ∅ token and U+1 for the initial state
+        self.decoder_output_shape = (seq_len+1, decoder_dim)  # (U+1, D)
+
+        self.decoder = Decoder(num_classes=num_vocab+1, output_dim=decoder_dim)
+        self.y = torch.randint(1, num_vocab, (seq_len+1, )).unsqueeze(0)  # (U+1, ), unsqueeze to add batch dimension
+        self.y[0][0] = 0
+
+    def test_one_step_forward(self):
+        g0, _ = self.decoder.step(self.y[:, 0], self.decoder.initial_state)
+        assert g0.shape == (1, self.decoder_output_shape[-1])
+
+    def test_sequnce_forward(self):
+        g = self.decoder(self.y)
+        assert g.shape == self.decoder_output_shape
